e442a9fcc6bfe9edcddc07171fa32516e0b7f8b3
Leglocker01 <beristaindelamora@gmail.com>
2024-04-25 16:22:07 -0600

    navbar para diseño desktop first

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+                background: '#ecf0f1',
+                principal: '#056CF2',
+                secondary: '#0540F2',
+                text: '#ecf0f1',
+                'text-secondary': '#000',
+                'button-color': '#A6033F',
+                'button-hover': '#BF2604'
+			}
+		},
 	},
 	plugins: [],
 }
